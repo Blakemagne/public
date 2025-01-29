42f86d46210685c56637923ff05aec6c1eb4f148
Blakemagne <bharrison.afhs@gmail.com>
2025-01-29 12:09:28 -0600

    added more nodes to print for testing to main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
+from leafnode import LeafNode
 
 def main():
     # Create a TextNode object
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
-
+    node = TextNode("This is text", TextType.BOLD, "https://www.boot.dev")
+    
+    # Create a PROPER HTMLNode with all required parameters
+    test_html = HTMLNode(
+        tag="div",
+        value="Hello World",
+        props={"class": "container"}
+    )
+    
+    # Create a LeafNode object
+    test_leaf = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+    
+    # Print PROPERLY using .to_html() and .__repr__()
+    print("TextNode:", node)
+    print("HTMLNode props:", test_html.props_to_html())
+    print("LeafNode HTML:", test_leaf.to_html())
 if __name__ == "__main__":
     main()
