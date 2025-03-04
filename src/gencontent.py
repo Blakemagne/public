6572adfcd01181fe6654c2d7499232d9b0e2cde9
Blakemagne <bharrison.afhs@gmail.com>
2025-03-04 15:42:18 -0600

    painful

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,50 +1,34 @@
-import unittest
-
-from gencontent import extract_title
-
-
-class TestExtractTitle(unittest.TestCase):
-    def test_eq(self):
-        actual = extract_title("# This is a title")
-        self.assertEqual(actual, "This is a title")
-
-    def test_eq_double(self):
-        actual = extract_title(
-            """
-# This is a title
-
-# This is a second title that should be ignored
-"""
-        )
-        self.assertEqual(actual, "This is a title")
-
-    def test_eq_long(self):
-        actual = extract_title(
-            """
-# title
-
-this is a bunch
-
-of text
-
-- and
-- a
-- list
-"""
-        )
-        self.assertEqual(actual, "title")
-
-    def test_none(self):
-        try:
-            extract_title(
-                """
-no title
-"""
-            )
-            self.fail("Should have raised an exception")
-        except Exception as e:
-            pass
+import os
+from markdown_blocks import markdown_to_html_node
 
 
-if __name__ == "__main__":
-    unittest.main()
+def generate_page(from_path, template_path, dest_path):
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
+
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
+
+
+def extract_title(md):
+    lines = md.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("no title found")
